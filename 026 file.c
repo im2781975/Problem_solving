bbfa3c690f92ce090ed44f5e16377e75717d7fcf
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-05 18:08:50 +0200

    Update 026 file.c

--- 026 file.c
+++ 026 file.c
@@ -1,5 +1,32 @@
 /***/
 #include<stdio.h>
+#include<string.h>
+int main()
+{
+    char input[1000];
+    int length, result,sum, ch, i;
+    while(scanf("%s", &input)!=EOF)
+    {
+        sum=0;
+        length= strlen(input);
+        for(i=0; i<length; i++)
+        {
+            ch=input[i];
+            if(ch>='a' && ch<='z')
+            {
+                sum=sum+ch-96;
+            }
+            else
+                sum=sum+ch-64+26;
+            sum=(result*10+input[i]-48)%5;
+            //printf("%d", input[i]-48);
+        }
+        printf("%d", result);
+    }
+
+}
+/***/
+#include<stdio.h>
 int main()
 {
 
