10579bd77714c0aa804918bf17bc1a03bd741d78
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-05 18:03:38 +0200

    Delete string4.c

--- string4.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    char country[] = {'B', 'A', 'N', 'G', 'L', 'A', 'D', 'E', 'S', 'H', '\0', 'I', 'S', ' ', 'M', 'Y', ' ', 'C', 'O', 'U', 'N', 'T', 'R', 'y'};
-        printf("%s\n", country);
-        return 0;
-}
