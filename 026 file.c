40102394d74b4787f23d56688d5682b9e19d352e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-05 18:14:11 +0200

    Update 026 file.c

--- 026 file.c
+++ 026 file.c
@@ -1,4 +1,36 @@
 /***/
+#include<stdio.h>
+int main()
+{
+int n, count=0;
+printf("enter an integer:");
+scanf("%d",&n);
+while(n!=0)
+{
+n/=10;
+++count;
+}
+printf("number of digits:%d", count);
+}
+/***/
+#include<stdio.h>
+int main()
+{
+   int ara[10]={1,2,3,4,5,6,7,8,9,10};
+   int temp;
+   temp=ara[5];
+   ara[5]=ara[7];
+   ara[7]=temp;
+        printf("%d",ara[7]);
+   //int i;
+   /*for(i=9;i>=0;i--){
+        printf("%d\n",ara[i]);
+   }*/
+
+
+   return 0;
+}
+/***/
 #include<bits/stdc++.h>
 using namespace std;
 int main()
