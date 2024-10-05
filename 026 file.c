d667606166658fe98c2034bd0b3c0bd760811b87
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-05 14:24:49 +0200

    Update 026 file.c

--- 026 file.c
+++ 026 file.c
@@ -490,7 +490,81 @@ int mian()
 
 }
 /***/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t, n, div, mod;
+    while(scanf("%d %d", &t, &n)!=EOF)
+    {
+    div= t/n;
+    mod=t%n;
+    printf("div=%d mod=%d\n", div, mod);
+    }
+    return 0;
 
+}
+/***/
+#include <stdio.h>
 
+int main()
+{
+  int n, array[1000], i, d, temp;
 
-//
+  printf("Enter number of elements\n");
+  scanf("%d", &n);
+
+  printf("Enter %d integers\n", n);
+
+  for (i = 0; i < n; i++) {
+    scanf("%d", &array[i]);
+  }
+
+  for (i = 1 ; i <= i - 1; i++) {
+    d = i;
+
+    while ( d > 0 && array[d] < array[d-1]) {
+      temp= array[d];
+      array[d]= array[d-1];
+      array[d-1]=temp;
+      d--;
+    }
+  }
+
+  printf("Sorted list in ascending order:\n");
+
+  for (i = 0; i <= n - 1; i++) {
+    printf("%d\n", array[i]);
+  }
+
+  return 0;
+}
+/***/
+#include<algorithm>
+#include<stdio.h>
+using namespace std;
+long int a[2000005],i,n;
+int main()
+{
+    while (scanf("%ld",&n)!=EOF)
+    {
+        if (n==0)
+        {
+            break;
+        }
+        for (i=0;i<n;i++)
+        {
+            scanf("%ld",&a[i]);
+        }
+        sort(a,a+n);
+        for (i=0;i<n;i++)
+        {
+            printf("%ld",a[i]);
+            if (i<(n-1))
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+/***
