bafc1ab125425f44029a7c9aa3073e10667e1e7b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-05 17:53:26 +0200

    Update 026 file.c

--- 026 file.c
+++ 026 file.c
@@ -1,3 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+  int num1,num2;
+
+  printf("Enter any two numbers\n");
+  scanf("%d%d",&num1,&num2);
+
+  num1 = num1 + num2;
+  num2 = num1 - num2;
+  num1 = num1 - num2;
+
+  printf("After swapping,the numbers are %d,%d\n",num1,num2);
+
+  return 0;
+}
+/***/
 #include<bits/stdc++.h>
 using namespace std;
 int main()
