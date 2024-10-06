8d124d86ec479f6fc2942bb9dc3a2f2a225f31e2
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-06 13:45:14 +0200

    Update problemset 55.c

--- problemset 55.c
+++ problemset 55.c
@@ -22,3 +22,15 @@ int main ()
     return 0;
 }
 /***/
+#include <stdio.h>
+int main ()
+{
+    int n= 5;
+    int i;
+    for (i = 1; i<=10; i = i + 1) {
+        printf ("%d* %d = %d\n",n, i,
+n*i);
+    }
+    return 0;
+}
+/***/
