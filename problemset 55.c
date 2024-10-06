683c37b2cd8e49c1b8a01f1d95f2aa91ecafc781
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-06 17:26:05 +0200

    Update problemset 55.c

--- problemset 55.c
+++ problemset 55.c
@@ -577,3 +577,25 @@ int main(){
   return 0;
 }
 /***/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a[1000], i, n;
+    scanf("%d", &n);
+    for(i=1; i<=n; i++)
+        scanf("%d", &a[i]);
+        printf("Odd Number : ");
+    for(i=1; i<=n; i++){
+        if(a[i]%2!=0)
+            printf("%d ", a[i]);
+            }
+            printf("\n");
+            printf("Even Number : ");
+              for(i=1; i<=n; i++){
+                if(a[i]%2==0)
+                    printf("%d ", a[i]);
+              }
+    return 0;
+}
+/***/
