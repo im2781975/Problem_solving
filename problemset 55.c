59d9befbb78458b3f3b9f70b2da3020e8cb0360a
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-06 13:52:55 +0200

    Update problemset 55.c

--- problemset 55.c
+++ problemset 55.c
@@ -120,3 +120,24 @@ int main()
     return 0;
     }
 /***/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=2, j, p;
+    while(i<=100){
+
+    p=1;
+    for(j=2; j<i; j++){
+        if(i%j==0){
+            p=0;
+            }
+        }
+        if(p){
+        printf("%d\n",i);
+        }
+    i++;
+    }
+    return 0;
+    }
+/***/
